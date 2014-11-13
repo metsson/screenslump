120bd1d952abe31d28eb11391f4205a3426b7347
metsson <merkur@live.se>
2014-11-13 23:00:24 +0100

    Canvas app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'slump#index'
 
+  post '/', to: 'slump#index'
+  
   get 'slump/:id/(:title)', to: 'slump#show', as: 'show_slump'
 
   get 'fetch', to: 'slump#fetch'
